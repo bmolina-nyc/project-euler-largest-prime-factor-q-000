010c89c647829cf56d6d03fc492196f0b0287967
bmolina-nyc <bruce.d.molina@gmail.com>
2016-02-27 18:14:49 -0500

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,24 @@
-# Enter your procedural solution here!
+
+
+def largest_prime_factor(number)
+
+    def prime?(num)
+     2.upto(num - 1).each { |el| return false if num % el == 0 }
+     true 
+    end
+
+
+    num = 2 
+    factor = 1 
+    array = [] 
+
+    while factor < number 
+      if number % num == 0 && prime?(num)
+        array << num 
+        factor *= num
+      end
+      num += 1 
+      break if num == number 
+    end
+    array.last 
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,36 @@
-# Enter your object-oriented solution here!
+ # Enter your object-oriented solution here!
+
+
+class LargestPrimeFactor
+
+  attr_reader :digit 
+ 
+  def initialize(digit)
+    @digit = digit
+  end
+
+  def prime?(digit)
+     2.upto(digit - 1).each { |el| return false if digit % el == 0 }
+     true 
+  end
+
+  def number
+    num = 2
+    factor = 1 
+    array = [] 
+
+    while factor < digit
+      if digit % num == 0 && prime?(num)
+        array << num 
+        factor *= num
+      end
+      num += 1 
+      break if num == digit
+    end
+    array.last 
+  end
+
+
+
+end 
+
